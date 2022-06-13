9a436ad1f008d5833c1964e708feab35507d40bf
philakagujones <philakagujones@gmail.com>
2022-06-12 20:50:31 -0700

    page work

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ function App() {
       <div className="logowrapper">
         <img src={logo} className="App-logo" alt="logo" />
       </div> 
+      <h1>Steps to Get Your Data From Snapchat</h1>
+      <div className='steps'>
+        <ul>
+          <li>Log into your account on <a href='https://accounts.snapchat.com/accounts/login?continue=%2Faccounts%2Fwelcome'>accounts.snapchat.com</a></li>
+          <li>Click <b>'My Data'</b></li>
+          <li>Click <b>'Submit Request'</b> at the bottom of the page</li>
+          <li>Enter a valid email address and we'll send you an email with a link once your data is ready to download</li>
+          <li>Follow the link in your email to download your data</li>
+          <li>Click the link to download your data</li>
+        </ul>
+      </div>
+      <h1>Steps to Easily Download All Your Memories</h1>
+      <div className='steps2'>
+        <ul>
+          <li>Log into your account on <a href='https://accounts.snapchat.com/accounts/login?continue=%2Faccounts%2Fwelcome'>accounts.snapchat.com</a></li>
+          <li>Click <b>'My Data'</b></li>
+          <li>Click <b>'Submit Request'</b> at the bottom of the page</li>
+          <li>Enter a valid email address and we'll send you an email with a link once your data is ready to download</li>
+          <li>Follow the link in your email to download your data</li>
+          <li>Click the link to download your data</li>
+        </ul>
+      </div>
+
+      <div className='upload'>
+
+      </div>
     </div>
   );
 }

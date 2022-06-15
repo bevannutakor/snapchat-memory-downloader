c1ea4582f21e36a06131a40d902681a182609745
bevannutakor <bevannutakor@icloud.com>
2022-06-14 23:21:32 -0700

    extracted aws file from app.snapchat

--- Server/main.js
+++ Server/main.js
@@ -1,4 +1,3 @@
-const https = require("https");
 const fs = require("fs");
 const path = require('path');
 const axios = require('axios');
@@ -9,31 +8,32 @@ const jsonFile = './memories_history.json';
 
 const downloads = require(jsonFile)["Saved Media"];
 
-async function getLinks(){
-    //let linksArray = []
-    for(var i=0; i < downloads.length; i++){
-        const fileTime = downloads[i]["Date"] //temp for now;
-        const fileName = createFileName(downloads[i], fileTime);
-        const fileUrl = downloads[i]["Download Link"];
-        const localFilePath = path.resolve(__dirname, 'Test', fileName);
-        try{
-            const response = await axios({
-                method: 'GET',
-                url: fileUrl,
-                responseType: 'stream'
-            })
-            const write = response.data.pipe(fs.createWriteStream(localFilePath));
-            write.on('finish', () => {
-                console.log('Successfully downloaded file!');
-              });
-        } catch (err){
-            throw new Error(err);
-        }
-    }
+function addLinkToFile(jsonData){
+    fs.writeFile(__dirname + '/test.json', JSON.stringify(jsonData), err => {
+        if (err) throw err
+        else console.log('nice');
+      })
+}
+//Extract aws link from app.snapchat.com
+async function extractAwsLink(){
+    let linksArray = [];
+    axios.defaults.timeout = 10000;
+    await Promise.all(
+        downloads.map(async (memory, index) => {
+            await axios.post(memory['Download Link'])
+                .then(res => {
+                    linksArray.push({'awsLink': res.data});
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+        })
+    )
+    addLinkToFile(linksArray);
 }
 
 function createFileName(download, fileTime){
-    var fileName = fileTime
+    let fileName = fileTime
     if(download["Media Type"].toLowerCase() == "image" || download["Media Type"].toLowerCase() == "photo"){
         fileName += ".jpg"
     } else if(download["Media Type"].toLowerCase() == "video"){
@@ -42,4 +42,8 @@ function createFileName(download, fileTime){
     return fileName;
 }
 
-getLinks();
+async function main(){
+    //loop through each aws link, and create a new file and add it to downloads
+    //then send it to the user
+    return null;
+}
